bbd630d5d5634d8e73df822ca99c0d3601d05622
AndersonSingh <andersonsingh@outlook.com>
2015-09-09 23:41:18 -0700

    added the ability for a user to create an account on the website and also login.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,17 +7,66 @@ var app = angular.module("application",["firebase", "ui.router"])
   $urlRouterProvider.otherwise("/register");
 
   $stateProvider
-    .state('home', {
+    .state("home", {
       url: "/home",
       templateUrl: "home.html"
     })
-    .state('register', {
+
+    .state("register", {
       url: "/register",
       templateUrl: "register.html"
     })
+
+    .state("login", {
+      url: "/login",
+      templateUrl: "login.html"
+    })
 }])
 
-.controller("registerCtrl",['$scope', function($scope){
 
+/* this controller will allow a user to register an account using an email and password. */
+
+.controller("registerCtrl",["$scope", function($scope){
+
+  $scope.user = {};
+
+  $scope.registerUser = function(){
+
+    var ref = new Firebase("https://comp3550a1.firebaseio.com");
+
+    ref.createUser($scope.user,function(error, userData){
+      if(error){
+        console.log("Error creating a user: " + error);
+      }
+      else{
+        console.log("Created a new user: " + userData.uid);
+      }
+    });
+
+  };
+
+}])
+
+/* this controller will be used to log a user into an account. */
+
+.controller("loginCtrl",["$scope", function($scope){
+
+  $scope.user = {};
+
+  $scope.loginUser = function(){
+
+    var ref = new Firebase("https://comp3550a1.firebaseio.com");
+
+    ref.authWithPassword($scope.user,function(error, authData){
+      if(error){
+        console.log("Failed to login user: " + error);
+      }
+      else{
+        console.log("Successfully logged in user");
+        console.log(authData);
+      }
+    });
+
+  };
 
 }])
