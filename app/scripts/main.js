72d8d83c912ab553d1706250dfa00c23522ba220
AndersonSingh <andersonsingh@outlook.com>
2015-09-09 21:06:56 -0700

    modified the web app to use angularjs and implemented routing. The template for the register page was also created.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,23 @@
-// jshint devel:true
-console.log('\'Allo \'Allo!');
+
+var app = angular.module("application",["firebase", "ui.router"])
+
+/* this block config all the application states. */
+.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider){
+
+  $urlRouterProvider.otherwise("/register");
+
+  $stateProvider
+    .state('home', {
+      url: "/home",
+      templateUrl: "home.html"
+    })
+    .state('register', {
+      url: "/register",
+      templateUrl: "register.html"
+    })
+}])
+
+.controller("registerCtrl",['$scope', function($scope){
+
+
+}])
