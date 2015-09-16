4f86c9f7dec65b567d53a6780a3a8169d10c8b53
Shiva <shivramx@gmail.com>
2015-09-15 21:30:31 -0400

    Added content to the suggestions page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -55,8 +55,8 @@ var app = angular.module("application",["firebase", "ui.router","zingchart-angul
 
   $scope.user = {};
 
+  /* this function will call on firebase to register a user account. */
   $scope.registerUser = function(){
-
     var ref = new Firebase("https://comp3550a1.firebaseio.com");
 
     ref.createUser($scope.user,function(error, userData){
@@ -67,86 +67,103 @@ var app = angular.module("application",["firebase", "ui.router","zingchart-angul
         console.log("Created a new user: " + userData.uid);
       }
     });
-
   };
 
 }])
 
-/* this controller will be used to log a user into an account. */
+/* this controller will log a user into an account using firebase authenication */
 
 .controller("loginCtrl",["$scope", "AuthService", function($scope, AuthService){
 
   $scope.user = {};
 
+  /* this function will call on firebase on authenicate a user using a custom service. */
   $scope.loginUser = function(){
-
     AuthService.setAuth($scope.user);
-
   };
+
 
 }])
 
 .controller("suggestionsCtrl", ["$scope", "$firebaseObject", "AuthService" , function($scope, $firebaseObject, AuthService){
 
+  /* initalize some variables. */
   $scope.location = {};
   $scope.users = {};
-
+  $scope.area = null;
 
   $scope.initSuggestions = function(){
 
+    /* create a reference object to the firebase database.*/
     var ref = new Firebase("https://comp3550a1.firebaseio.com");
 
-    navigator.geolocation.getCurrentPosition(function(position){
+    /* create a reference object to firebase users. */
+    var usersRef = new Firebase("https://comp3550a1.firebaseio.com/users");
+    $scope.users = $firebaseObject(usersRef);
 
+    /* use the HTML5 geolocation feature to get the users lat/long coords. */
+    navigator.geolocation.getCurrentPosition(function(position){
       $scope.location.lng = position.coords.longitude;
       $scope.location.lat = position.coords.latitude;
 
-      console.log($scope.location);
-
+      /* we need to convert the lat/long value to a city/ town using google's reverse geocoding. */
       var geocoder = new google.maps.Geocoder();
       var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
 
-
       geocoder.geocode({'latLng': $scope.location}, function(results, status) {
 	       if (status == google.maps.GeocoderStatus.OK) {
-
            /* decode city/town here to organize vendors. */
-
            $scope.area = results[0].address_components[1].short_name;
-
            /* update the city list. */
            ref.child("areas").child($scope.area).set(true);
+         }
+
+         /* if area was not detected, display alert accordingly. */
+         $scope.area = null;
+         if($scope.area == null){
+           $scope.area_alert = "Sorry, Your Area Could Not Be Identified, Loading Suggestions From All Areas.";
+
+           /* since we could not find an area, load all suggestions. */
+           var suggestionsRef = new Firebase("https://comp3550a1.firebaseio.com/suggestions");
+           $scope.suggestions = $firebaseObject(suggestionsRef);
+         }
+         else{
+           $scope.area_alert = "Loading Suggestions From " + $scope.area + " And Surrounding Areas.";
+
+           /* now that we have an area, we can load suggestions for this area. */
+           var suggestionsRef = new Firebase("https://comp3550a1.firebaseio.com/suggestions/" + $scope.area);
+           $scope.suggestions = $firebaseObject(suggestionsRef);
 
          }
-       });
-    });
+
+
+
+       }); /* end geocode block */
+    }); /* end navigation block. */
 
   };
-
-  $scope.getSuggestions = function(){
-
-    var ref = new Firebase("https://comp3550a1.firebaseio.com/suggestions/Tunapuna/");
-    $scope.suggestions = $firebaseObject(ref);
-    console.log($scope.suggestions);
-
-  };
-
-  /* this function will pull all the data for each user from firebase. */
-  $scope.getUsers = function(){
-
-    var ref = new Firebase("https://comp3550a1.firebaseio.com/users");
-    $scope.users = $firebaseObject(ref);
-  }
 
   $scope.postSuggestion = function(){
-
     /* make a refernce to the firebase database.. */
     var ref = new Firebase("https://comp3550a1.firebaseio.com");
-
     /* retrieve the user id, of the person posting this suggestion. */
-    var id = AuthService.getAuth().uid;
-
-    ref.child("suggestions").child($scope.area).push({text : $scope.suggestion, name : id});
+    var auth = AuthService.getAuth();
+    var id = null;
+    /* valid id. */
+    if(auth != null){
+      id = auth.uid;
+      if($scope.area == null){
+        var temp  = "unknown";
+        ref.child("suggestions").child(temp).push({text : $scope.suggestion, name : id});
+      }
+      else
+      {
+        ref.child("suggestions").child($scope.area).push({text : $scope.suggestion, name : id});
+      }
+    }
+    else{
+      console.log("You cant post because you aren't logged in.");
+    }
 
   };
 
@@ -155,7 +172,7 @@ var app = angular.module("application",["firebase", "ui.router","zingchart-angul
 
 /* this is an authenication service, that will make the auth info available to all pages that request it. */
 
-.service("AuthService",[function(){
+.service("AuthService",["$state", function($state){
 
   var auth = null;
 
@@ -180,6 +197,9 @@ var app = angular.module("application",["firebase", "ui.router","zingchart-angul
             name: authData.password.email,
             profileImageURL : authData.password.profileImageURL
           });
+
+          /* redirect user after successfully logging in.*/
+          $state.go("home");
 
         }
       });
